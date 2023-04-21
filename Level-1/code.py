2ae7164a5c6d1f92bf4ed1b62a7fba276fed21b3
Preston N <prestonnyamweya@gmail.com>
2023-04-21 13:07:46 +0000

    fix

--- Level-1/code.py
+++ Level-1/code.py
@@ -1,7 +1,7 @@
 '''
 ////////////////////////////////////////////////////////////
 ///                                                      ///
-///   0. tests.py is passing but the code is vulnerable  /// 
+///   0. tests.py is passing but the code is vulnerable  ///
 ///   1. Review the code. Can you spot the bug?          ///
 ///   2. Fix the code but ensure that tests.py passes    ///
 ///   3. Run hack.py and if passing then CONGRATS!       ///
@@ -12,22 +12,33 @@
 '''
 
 from collections import namedtuple
+from decimal import Decimal
 
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
-def validorder(order: Order):
-    net = 0
-    
+MAX_ITEM_AMOUNT = 100000  # maximum price of item in the shop
+MAX_QUANTITY = 100  # maximum quantity of an item in the shop
+MAX_TOTAL = 1e6  # maximum total amount accepted for an order
+
+
+def validorder(order):
+    net = Decimal('0')
+
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            # sets a reasonable min & max value for the invoice amounts
+            if item.amount > -1*MAX_ITEM_AMOUNT and item.amount < MAX_ITEM_AMOUNT:
+                net += Decimal(str(item.amount))
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            if item.quantity > 0 and item.quantity <= MAX_QUANTITY and item.amount > 0 and item.amount <= MAX_ITEM_AMOUNT:
+                net -= Decimal(str(item.amount)) * item.quantity
+            if net > MAX_TOTAL or net < -1*MAX_TOTAL:
+                return ("Total amount exceeded")
         else:
-            return("Invalid item type: %s" % item.type)
-    
+            return ("Invalid item type: %s" % item.type)
+
     if net != 0:
-        return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
+        return ("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
     else:
-        return("Order ID: %s - Full payment received!" % order.id)
+        return ("Order ID: %s - Full payment received!" % order.id)
---
+++ Level-1/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+def validorder(order: Order):
